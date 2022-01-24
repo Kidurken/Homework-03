69704483949608e54945632787f78dbcc8fd19ff
Kidurken <jpatchen2000@gmail.com>
2022-01-24 18:12:55 -0500

    making it actually work

--- script.js
+++ script.js
@@ -1,31 +1,165 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
-var characters = [""];
+
+
+var specialCharacters = [
+  '@',
+  ':',
+  ',',
+  ')',
+  '(',
+  '}',
+  '{',
+  ']',
+  '[',
+  '~',
+  '-',
+  '_',
+  '.',
+  '%',
+  '+',
+  '\\',
+  '/',
+  "'",
+  '!',
+  '#',
+  '$',
+  '^',
+  '?'
+];
+
+var numerals = ['0','1','2','3','4','5','6','7','8','9'];
+
+var lowercaseCharacters = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z'
+]
+
+var upperCaseCharacters = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z'
+]
+
+function getOptions() {
+  length = prompt("how many characters do you want?(8 to 128)");
+  uppercase = confirm("Do you want uppercase characters?");
+  lowercase = confirm("Do you want lowercase characters?");
+  numeric = confirm("Do you want numbers?");
+  specials = confirm("do you want special characters");
+
+  if (length < 8 || length > 128){
+    length = prompt("please enter a valid number (8 to 128)");
+    return null;
+  } 
+
+  var passwordOptions = {
+    length: length,
+    uppercase: uppercase,
+    lowercase: lowercase,
+    numeric: numeric,
+    specials: specials,
+  }
+  return passwordOptions;
+}
+
+function getRandom(arr) {
+  var randIndex = Math.floor(Math.random() * arr.length);
+  var randElement = arr[randIndex];
+
+  return randElement;
+}
 
 function generatePassword() {
-uppercase = window.confirm("Do you want uppercase characters?");
-lowercase = window.confirm("Do you want lowercase characters?");
-numeric = window.confirm("Do you weant numbers?");
-specialCharacters = window.confirm("do you want special characters");
-length = prompt("how many characters do you want?(8 to 128)");
-if (length < 8 || legth > 128){
-  length = prompt("please enter a valid number (8 to 128)");
-  return;
-} 
-if (uppercase = true){
-  characters.concat(["ABCDEFGHIJKLMNOPQRSTUVWXYZ"]);
+  var options = getOptions();
+
+  var result = [];
+
+  var possibleCharacters = [];
+
+  var guaranteedCharacters = [];
+
+  if (options.specials) {
+    possibleCharacters = possibleCharacters.concat(specialCharacters);
+    guaranteedCharacters.push(getRandom(specialCharacters));
+  }
+
+  if (options.numeric) {
+    possibleCharacters = possibleCharacters.concat(numerals);
+    guaranteedCharacters.push(getRandom(numerals));
+  }
+
+  if (options.lowercase) {
+    possibleCharacters = possibleCharacters.concat(lowercaseCharacters);
+    guaranteedCharacters.push(getRandom(lowercaseCharacters));
+  }
+
+  if (options.hasUpperCasedCharacters) {
+    possibleCharacters = possibleCharacters.concat(upperCaseCharacters);
+    guaranteedCharacters.push(getRandom(upperCaseCharacters));
+  }
+
+  for (var i = 0; i < options.length; i++) {
+    var possibleCharacter = getRandom(possibleCharacters);
+
+    result.push(possibleCharacter);
+  }
+
+  for (var i = 0; i < guaranteedCharacters.length; i++) {
+    result[i] = guaranteedCharacters[i];
+  }
+
+  return result.join('');
+
 }
-if (lowercase = true){
-  characters.concat(["abcdefghijklmnopqrstuvwxyz"]);
-}
-if (numeric = true){
-  characters.concat(["0123456789"]);
-}
-if (specialCharacters = true){
-  characters.concat(["!@#$%^&*()/*-+"]);
-}
-console.log(characters);
-}
+
+var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
